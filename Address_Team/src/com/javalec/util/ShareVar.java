25bdcd0619f8c9433293b16d8414af4c363c71d4
Jty98 <130356276+Jty98@users.noreply.github.com>
2023-05-03 18:25:18 +0900

    util

--- Address_Team/src/com/javalec/util/ShareVar.java
+++ Address_Team/src/com/javalec/util/ShareVar.java
@@ -1,5 +1,8 @@
 package com.javalec.util;
 
-public class ShareVar {
-
+public class SHareVar {
+	// 공통적으로 같이 사용이라 public static
+	public static final String DBName = "jdbc:mysql://127.0.0.1/useraddress?serverTimezone=UTC&characterEncoding=utf8&useSSL=FALSE"; 
+	public static final String DBUser = "root";
+	public static final String DBPass = "qwer1234";
 }
