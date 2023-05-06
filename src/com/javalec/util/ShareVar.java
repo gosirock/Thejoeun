e6a79cdf9ae2f0b1c7383651a6ef353eb72ae223
wookhyunKim <kwh1041@naver.com>
2023-05-06 19:47:37 +0900

    add admin

--- src/com/javalec/util/ShareVar.java
+++ src/com/javalec/util/ShareVar.java
@@ -3,7 +3,7 @@ package com.javalec.util;
 import java.util.ArrayList;
 
 public class ShareVar {
-	public static final String DBName = "jdbc:mysql://127.0.0.1/shoes?serverTimezone=UTC&characterEncoding=utf8&useSSL=FALSE";
+	public static final String DBName = "jdbc:mysql://127.0.0.1/shoesshop?serverTimezone=UTC&characterEncoding=utf8&useSSL=FALSE";
 	public static final String DBUser = "root";
 	public static final String DBPass = "qwer1234";
 	public static String loginUserId = "";
